3a86473f7d2482baae4101eaf5f6387e1b5adeb5
CristianOlivera1 <221181@unamba.edu.pe>
2025-01-28 09:48:29 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+import matplotlib.pyplot as plt
+import io
+import base64
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/northwest_corner', methods=['GET', 'POST'])
+def northwest_corner():
+    if request.method == 'POST':
+        input1 = request.form['input1']
+        input2 = request.form['input2']
+        # Aquí implementa la lógica del algoritmo Esquina del Noroeste usando input1 y input2
+        fig, ax = plt.subplots()
+        ax.plot([1, 2, 3], [1, 4, 9])  # Reemplaza esto con la lógica del algoritmo
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+        plot_url = base64.b64encode(img.getvalue()).decode()
+        return render_template('northwest_corner.html', plot_url=plot_url)
+    return render_template('northwest_corner.html')
+
+@app.route('/other_algorithm', methods=['GET', 'POST'])
+def other_algorithm():
+    if request.method == 'POST':
+        # Implementa aquí otro algoritmo y genera su gráfica
+        pass
+    return render_template('other_algorithm.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
